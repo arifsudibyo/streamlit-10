02bfc55ff8c75b947e2e16d25b45b137f3a4d7e5
william sanjaya <35284667+arifsudibyo@users.noreply.github.com>
2021-10-24 06:06:42 +0700

    Update sp500-app.py

--- sp500-app.py
+++ sp500-app.py
@@ -43,7 +43,7 @@ st.dataframe(df_selected_sector)
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="SP500_.csv">Download CSV File</a>'
     return href
 
 st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
